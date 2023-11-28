2581a570b48fc486529b0ce15c6484a838e439c1
Anton <reopik@mail.ru>
2023-11-28 16:08:37 +0300

    created some structure in App.tsx for some components

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+const App = () => {
+  return (
+    <Router>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route  path="/video/:id" element={<VideoDetail />} />
+          <Route  path="/channel/:id" element={<ChannelDetail />} />
+          <Route  path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </Router>
+  )
+}
+
+export default App;
