7b44197ffc16caad091a2e50356b01b19341b29b
Anton <reopik@mail.ru>
2023-11-29 15:50:49 +0300

    created and styled sidebar

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,24 @@
-import React, { FC } from 'react'
+import { FC } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
 
 const Feed: FC = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}>
+        <Sidebar />
 
-export default Feed
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright 2023 JSM Media
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
