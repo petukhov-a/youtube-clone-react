cc5d50a3e801f9ada8727d43e9958322b9a6498f
Anton <reopik@mail.ru>
2023-11-30 18:14:41 +0300

    showing name of the videos and channels under thumbnails

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -2,8 +2,9 @@ import { FC } from 'react';
 import { ApiItem } from '../utils/fetchFromAPI';
 
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
-import { Card, CardMedia } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { CheckCircle, Snooze } from '@mui/icons-material';
 
 type VideoCardProps = {
   video: ApiItem;
@@ -13,7 +14,7 @@ const VideoCard: FC<VideoCardProps> = ({ video }) => {
   const { id: { videoId }, snippet } = video;
 
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: '0', borderRadius: 'none' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url as string}
@@ -22,6 +23,21 @@ const VideoCard: FC<VideoCardProps> = ({ video }) => {
           component='img'
         />
       </Link>
+      <CardContent  sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant='subtitle1' fontWeight="bold" color="#fff">
+            {snippet?.title.slice(0, 60) ||
+             demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant='subtitle2' fontWeight="bold" color="gray">
+            {snippet?.channelTitle ||
+             demoChannelTitle}
+             <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   )
 }
